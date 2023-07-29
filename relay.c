c240c73785145f0f462d89de65f1538afde7bbfb
Michael Wilkinson <michael@wilkinsons.me.uk>
2023-07-29 14:08:53 +0100

    Timer and character input
    Blinking now done via repeating timer.
    Single character input and echo

--- relay.c
+++ relay.c
@@ -5,6 +5,21 @@
 
 const uint LED_PIN = 25;
 
+bool blink_led_on = false;
+
+bool blink_callback(struct repeating_timer *t) {
+    if (blink_led_on) {
+        gpio_put(LED_PIN, 1);
+        puts("Hello World\n");
+        blink_led_on = false;
+    }
+    else {
+        gpio_put(LED_PIN, 0);
+        blink_led_on = true;
+    }
+    return true;
+}
+
 int main() {
 
     bi_decl(bi_program_description("This is a test binary."));
@@ -14,12 +29,15 @@ int main() {
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
 
+    struct repeating_timer timer;
+    add_repeating_timer_ms(500, blink_callback, NULL, &timer);
+
+    int16_t ch = getchar_timeout_us(100);
+    while (1) {
+        ch = getchar_timeout_us(100);
+        printf("%c", ch); 
+        
     sleep_ms(1000);
     }
 }
