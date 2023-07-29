bc069a1fa837b02d00a342f39d4152665620a333
Michael Wilkinson <michael@wilkinsons.me.uk>
2023-07-29 17:03:34 +0100

    Read in lines from USB
    Pretty basic and might be ignoring first line.
    Next step break line into words

--- relay.c
+++ relay.c
@@ -2,10 +2,13 @@
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include "relay.h"
 
 const uint LED_PIN = 25;
 
 bool blink_led_on = false;
+char line_buffer[MAX_LINE_LENGTH + 1];
+
 
 bool blink_callback(struct repeating_timer *t) {
     if (blink_led_on) {
@@ -18,6 +21,31 @@ bool blink_callback(struct repeating_timer *t) {
         blink_led_on = true;
     }
     return true;
+}
+
+bool isEqual(const char *cmd, const char *buffer)
+{
+    return strcmp(buffer, cmd) == 0;
+}
+
+int readLine(char *buffer, int length)
+{
+    // https://lindevs.com/turn-on-off-the-onboard-led-on-raspberry-pi-pico-via-usb
+    int index = 0;
+    while (index < length) {
+        int c = getchar_timeout_us(100);
+        if (c < 0) {
+            continue;
+        }
+        buffer[index++] = (char) c;
+        if (c == '\n') {
+            break;
+        }
+
+    }
+    buffer[index] = '\0';
+
+    return index;
 }
 
 int main() {
@@ -33,12 +61,13 @@ int main() {
     struct repeating_timer timer;
     add_repeating_timer_ms(500, blink_callback, NULL, &timer);
 
-    int16_t ch = getchar_timeout_us(100);
     while (1) {
-        ch = getchar_timeout_us(100);
-        printf("%c", ch); 
+        uint16_t data_len = readLine(line_buffer, MAX_LINE_LENGTH);
+        if (data_len > 0) {
+            printf("%s", line_buffer);
+        }
+    }
         
     sleep_ms(1000);
-    }
 }
 
